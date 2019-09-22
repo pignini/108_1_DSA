e7ab9b8539c9031ae055e11aa710030d4b4fe9ab
Wen Sui (Ellier) <okpersist@gmail.com>
2019-09-22 15:38:20 +0800

    Update design_linked-list.py

--- week2/design_linked-list.py
+++ week2/design_linked-list.py
@@ -1,9 +1,9 @@
 # [Design Linked list](https://leetcode.com/problems/design-linked-list/)
 
-class Node:
+class Node: #創建一個節點分類
     def __init__(self, val=None):
-        self.val = val
-        self.next = None
+        self.val = val #節點分類屬性val，是節點的值
+        self.next = None　#節點分類屬性nexr，是節點下一個指向的值
 
 
 class MyLinkedList:
@@ -29,8 +29,10 @@ class MyLinkedList:
         """
         Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
         """
-        
-        
+        new_node = node(val) #建立一個新節點
+        new_node.next = self.head #把原本的頭節點設定成新節點的下一個
+        self.head = new_node #把新節點設立成頭節點
+         
     #count node
         current_node = 0
         while self.next != 0:
@@ -44,9 +46,12 @@ class MyLinkedList:
         """
         Append a node of value val to the last element of the linked list.
         """
-        self.val = val
-        self.next = None
+        new_node = node(val)
+        if self.next is None:
+            self.next = new_node
+            return
 
+        
     def addAtIndex(self, index: int, val: int) -> None:
         """
         Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
