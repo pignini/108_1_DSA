d64c8d8a6eb22964e5662804563931a66abe9279
Wen Sui (Ellier) <okpersist@gmail.com>
2019-09-23 21:50:43 +0800

    Update design_linked-list.py

--- week2/design_linked-list.py
+++ week2/design_linked-list.py
@@ -1,19 +1,28 @@
 # [Design Linked list](https://leetcode.com/problems/design-linked-list/)
 
 class Node: #創建一個節點分類
-class Node: #創建一個節點分類
     def __init__(self, val=None):
         self.val = val #節點分類屬性val，是節點的值
-        self.next = None　#節點分類屬性nexr，是節點下一個指向的值
-
-
+        self.next = None
+        
+# class count:
+#     def __init__(self):
+#         cur_index = 1
+#         cur_node =self.head
+#         while cur_node.next != None:
+#             cur_index += 1
+#         return cur_index
+        
 class MyLinkedList:
-    index = 0
+
     def __init__(self):
         """
         Initialize your data structure here.
         """
         self.head = Node()
+        
+        
+   
     
     def get(self, index: int) -> int:
         """
@@ -26,13 +35,12 @@ class MyLinkedList:
             curr_node = curr_node.next
             print(curr_node.val)
             return
-        
 
     def addAtHead(self, val: int) -> None:
         """
         Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
         """
-        new_node = node(val) #建立一個新節點
+        new_node = Node(val) #建立一個新節點
         new_node.next = self.head #把原本的頭節點設定成新節點的下一個
         self.head = new_node #把新節點設立成頭節點
          
@@ -41,7 +49,7 @@ class MyLinkedList:
         """
         Append a node of value val to the last element of the linked list.
         """
-        new_node = node(val) #建立一個新的節點
+        new_node = Node(val) #建立一個新的節點
         if self.head is None: #當第一個節點是空的，代表目前沒有節點在list中
             self.head = new_node #把第一個節點令成新的節點
             return
@@ -56,28 +64,43 @@ class MyLinkedList:
         """
         Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
         """
-        new_node = node(val)
-        curr_index = 0 
+        new_node = Node(val)
+        curr_index = 1
         curr_node = self.head
-        if index == len(linkedlList):
+        cur_node = self.head
+        
+        count_index = 1
+        while cur_node.next != None:
+            count_index += 1
+            return
+        
+        if index == count_index:
             addAtTail(index)
-        elif index > len(linkedList):
+        elif index > count_index:
             pass
         else:
             while curr_index != index:
                 curr_index += 1
                 curr_node = curr_node.next
+            pre_node = curr_node
             old_node = curr_node.next
-            curr_node.next = new_node
+            pre_node.next = new_node
             new_node.next = old_node
 
     def deleteAtIndex(self, index: int) -> None:
         """
         Delete the index-th node in the linked list, if the index is valid.
         """
-         if index >= len(linkedList):
-            break
-        elif:
+        cur_node = self.head
+        
+        count_index = 1
+        while cur_node.next != None:
+            count_index += 1
+            return
+        
+        if index >= count_index:
+            pass
+        else:
             cur_index = 1
             cur_index2 = -1
             cur_node = self.head
@@ -92,7 +115,7 @@ class MyLinkedList:
             pre_node = cur_node
             back_node = cur_node2
             pre_node.next = back_node
-
+            
 
 # Your MyLinkedList object will be instantiated and called as such:
 # obj = MyLinkedList()
