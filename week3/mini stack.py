85eaedff01c929aaae3b1d2df8d65d14c895b2ce
Wen Sui (Ellier) <okpersist@gmail.com>
2019-09-27 11:19:16 +0800

    Create mini stack.py

---
+++ week3/mini stack.py
@@ -0,0 +1,31 @@
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.val = val
+        self.size = 0
+        self.next = None
+
+    def push(self, x: int) -> None:
+        if self.size = 0:
+            head = self.val
+            self.size += 1
+
+    def pop(self) -> None:
+        
+
+    def top(self) -> int:
+        
+
+    def getMin(self) -> int:
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
