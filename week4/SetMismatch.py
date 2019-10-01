15e143c4b4d0475c5a621566bc33ba53c05702e6
Wen Sui (Ellier) <okpersist@gmail.com>
2019-10-01 12:03:41 +0800

    Update SetMismatch.py

--- week4/SetMismatch.py
+++ week4/SetMismatch.py
@@ -1,3 +1,30 @@
 # Github-645-easy
 
+def setMismatch():
+    num = sorted(num)
+    index = 0
+    missing = []
+    repeat = []
+    inlist = []
+    output = []
 
+    for i in range(len(num)):
+        if i == index+1:
+            index += 1
+        elif num == index:
+            repeat.append(index)
+            missing.append(index+1)
+            index += 1
+        else num > index+1:
+            if num > len(num):
+                missing.append(index+1)
+                index += 1
+            else:
+                missing.append(index+1)
+                inlist.append(num)
+                index += 1
+
+    repeat.extend(inlist)
+    repeat = set(repeat)
+    missing = set(missing)
+    return repeat, missing
